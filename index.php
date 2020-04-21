ef49267d5e22801d1ad9ce91025478d7b9c8fecd
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-04-21 09:50:11 +0200

    feature #5 (project): deletion of the test code

--- index.php
+++ index.php
@@ -6,9 +6,6 @@ require_once('core/Request.php');
 
 try{
     $request = Request::getInstance();
-    var_dump($request);
 } catch (Exception $e) {
     echo 'Exception  reçue : ' . $e->getMessage();
-} catch (TypeError $e) {
-    echo 'Exception ' . $e->getMessage();
 }
