3dec1f710be93f9e79bfe78ccd45c238c86620f4
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-04-21 11:13:23 +0200

    feature(project): changing the name of Router class file and testing in index

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 
 use Core\Request;
+use Core\Router;
 
 require_once('core/Request.php');
+require_once('core/Router.php');
 
 try{
     $request = Request::getInstance();
+    $router = new Router();
 } catch (Exception $e) {
     echo 'Exception  reçue : ' . $e->getMessage();
 }
